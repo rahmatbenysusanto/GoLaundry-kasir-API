b78951728feb0dbfbfbc22bf2dd7468371f0d6f1
rahmatbenysusanto <rahmat.beny12@gmail.com>
2023-03-10 21:00:17 +0700

    create order

--- src/controllers/orderController.js
+++ src/controllers/orderController.js
@@ -2,6 +2,7 @@ import order from '../models/order.js';
 import orderDetail from "../models/orderDetail.js";
 import orderHistory from "../models/orderHistory.js";
 import {responseNotData} from "../services/responseService.js";
+import {create} from "./apiKeyController.js";
 
 export const createOrder = async (req, res) => {
     try {
@@ -11,8 +12,29 @@ export const createOrder = async (req, res) => {
             price: req.body.price,
             status: 'new'
         });
-        responseNotData(res, true, 'Create Order Successfully', 201);
+
+        // Create Order Detail
+        for(let a = 0; a < req.body.detail.length; a++) {
+            await orderDetail.create({
+                user_id: req.body.detail[a].user_id,
+                client_id: req.body.detail[a].client_id,
+                order_id: createOrder.id,
+                layanan_id: req.body.detail[a].layanan_id,
+                price: req.body.detail[a].price,
+                count : req.body.detail[a].count
+            });
+        }
+
+        // Create Order History
+        await orderHistory.create({
+            order_id : createOrder.id,
+            title_history: 'Create Order',
+            desc_history : 'Order berhasil dibuat'
+        });
+        responseNotData(res, true, 'Order Create Successfully', 201);
     } catch (err) {
         responseNotData(res, false, err.message, 400);
     }
 }
+
+
